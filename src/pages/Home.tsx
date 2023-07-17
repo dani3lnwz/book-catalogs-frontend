ec8fd2353b01d1ec8e6bfdf7e6b928734514740f
Daniel Newaz <danielnwz420@gmail.com>
2023-07-17 13:32:22 +0600

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Hero from "../components/ui/Hero";
+import Footer from "./Footer";
+import TopBooks from "./TopBooks";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <TopBooks />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
